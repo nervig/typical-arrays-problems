a35d86b9efaed83cc7667daf6b7b99089d23ae2b
nervig <solide@yandex.ru>
2021-03-11 22:28:21 +0300

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined){
+        return 0;
+    }
+    else if (array.length == 0){
+        return 0;
+    } else {
+        let temp_item = array[0];
+        array.forEach(element => {
+            if (element < temp_item){
+                temp_item = element;
+            }
+        });
+        return temp_item;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    let temp_item = 0;
+    if (array == undefined){
+        return 0;
+    }
+    else if (array.length == 0){
+        return 0;
+    } else {
+        array.forEach(element => {
+            if (element > temp_item){
+                temp_item = element;
+            }
+        });
+    }  
+    return temp_item;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let temp_item = 0;
+    if (array == undefined){
+        return 0;
+    }
+    else if (array.length == 0){
+        return 0;
+    }   else {
+        array.forEach(element => {
+            temp_item += element; 
+         });
+    }   
+    return temp_item / array.length;
 }
